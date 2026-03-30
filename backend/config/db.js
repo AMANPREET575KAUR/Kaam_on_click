bb17da5017ce2c892bc2c69cd7805ab62826a01b
Arjit Pandey <pandeyarjit240@gmail.com>
2026-03-30 14:06:40 +0530

    Temp: saving changes before switching branch

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(
   "kaam_on_click",
   "root",
-  "Apoorva@1",
+  "Pandey",
   {
     host: "localhost",
     dialect: "mysql",
